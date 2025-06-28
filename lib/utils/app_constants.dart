9a60a9b3a6044541c64af7ef6348ef363109757b
sanjay <sanjay05021992@gmail.com>
2025-06-28 23:56:46 +0530

    000001A nothing special. removed two files

--- lib/utils/app_constants.dart
+++
@@ -1,5 +0,0 @@
-// lib/utils/app_constants.dart
-class AppConstants {
-  static const String appName = 'Substation Manager'; // Updated app name
-  // You can add other local constants here if needed.
-}
